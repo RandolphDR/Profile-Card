05d6ec164bf13b3fba03abb9b90dc98474765b57
ArtuDR <randolphsaludo22@gmail.com>
2024-09-03 23:46:55 +0800

    Initial commit

---
+++ source/profilecard.js
@@ -0,0 +1,6 @@
+function follow_btn() {
+
+    alert("Thanks for following! \nThis will redirect to my Official Github Profile");
+    let btn = document.getElementById("follow");
+    btn.innerHTML = "Followed";
+}
